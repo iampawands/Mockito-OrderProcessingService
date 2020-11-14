154e5e25a403287b489854812fa0912fb8d2a898
iampawands <iam.pawands@gmail.com>
2020-11-14 16:31:16 +0530

    partial mocking using Mockito.when(list.get(2)).thenCallRealMethod();

--- test/com/pawan/mockito/spy/ListTest.java
+++ test/com/pawan/mockito/spy/ListTest.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
 
@@ -12,7 +14,7 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
 public class ListTest {
-	@Spy
+	@Mock
 	List<String> list = new ArrayList<String>();
 	
 	@Before
@@ -24,7 +26,10 @@ public class ListTest {
 	public void listTestCase() {
 //		when(list.get(2)).thenReturn("Hello"); --> this will throw array out of bound exception because spy will call all method of
 //		mock object. So @Mock will work fine. But if want to use @Spy then use below doReturn type of stubbing
-		doReturn("Hello").when(list).get(2);
+//		doReturn("Hello").when(list).get(2);
+		
+//		if we want to do partial mocking using the @Mock itself then use below
+		Mockito.when(list.get(2)).thenCallRealMethod();
 		String result = list.get(2);
 		assertEquals("Hello", result);
 	}
